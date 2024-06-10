595f9a4d3d2c7d4528018d621ac973b07a3c30da
Nkunja <munene01denis@gmail.com>
2024-06-10 11:47:26 +0300

    using images

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Container, Typography, Button } from '@material-ui/core';
+import { Container, CardMedia, Typography, Button } from '@material-ui/core';
 import { Link } from 'react-router-dom';
+import images from './imageData';
 
 const Home = () => {
   return (
@@ -8,6 +9,12 @@ const Home = () => {
       <Typography variant="h4" gutterBottom>
         Book Viewer
       </Typography>
+        <CardMedia
+          component="img"
+          alt="welcome"
+          height="140"
+          image={images[2]}
+        />
       <Typography variant="body1" gutterBottom>
         This is the home page. You can navigate to the book assignment view to start assigning books to your reading list.
       </Typography>
