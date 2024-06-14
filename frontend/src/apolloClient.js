6a1833b80ec6ebed982f38b802b30ae14099912a
Nkunja <munene01denis@gmail.com>
2024-06-14 11:32:04 +0300

    HttpLink Require

--- frontend/src/apolloClient.js
+++ frontend/src/apolloClient.js
@@ -1,46 +1,13 @@
-// import { ApolloClient, InMemoryCache} from '@apollo/client';
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
-// const client = new ApolloClient({
-//   uri: 'http://localhost:4000/',
-//   cache: new InMemoryCache()
-// });
-
-// export default client;
-import { ApolloClient, InMemoryCache, ApolloLink, HttpLink } from '@apollo/client';
-import { createUploadLink } from 'apollo-link-http';
-
-const onlineApiLink = createUploadLink({
-  uri: 'https://ellochallenge.onrender.com/',
-  credentials: 'same-origin',
+const httpLink = new HttpLink({
+  uri: process.env.REACT_APP_GRAPHQL_ENDPOINT,
 });
 
-const localApiLink = new HttpLink({
-  uri: 'http://localhost:4000/graphql',
-});
-
-const link = ApolloLink.split(
-  () => process.env.NODE_ENV === 'production',
-  onlineApiLink,
-  localApiLink
-);
 
 const client = new ApolloClient({
-  link,
+  link: httpLink,
   cache: new InMemoryCache(),
 });
 
 export default client;
-
-// import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
-
-// const httpLink = new HttpLink({
-//   uri: process.env.REACT_APP_GRAPHQL_ENDPOINT,
-// });
-
-
-// const client = new ApolloClient({
-//   link: httpLink,
-//   cache: new InMemoryCache(),
-// });
-
-// export default client;
