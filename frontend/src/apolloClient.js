652a60379d2ff90767ab1930847cb2758607ef22
Nkunja <munene01denis@gmail.com>
2024-06-14 10:29:03 +0300

    Adding production url

--- frontend/src/apolloClient.js
+++ frontend/src/apolloClient.js
@@ -1,8 +1,22 @@
-import { ApolloClient, InMemoryCache} from '@apollo/client';
+// import { ApolloClient, InMemoryCache} from '@apollo/client';
+
+// const client = new ApolloClient({
+//   uri: 'http://localhost:4000/',
+//   cache: new InMemoryCache()
+// });
+
+// export default client;
+
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+
+const httpLink = new HttpLink({
+  uri: process.env.REACT_APP_GRAPHQL_ENDPOINT,
+});
+
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/',
-  cache: new InMemoryCache()
+  link: httpLink,
+  cache: new InMemoryCache(),
 });
 
 export default client;
