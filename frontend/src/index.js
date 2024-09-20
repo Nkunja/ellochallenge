c6ef7873169eab93d8150c1cc195942222e2f378
Denis Nkunja <67855162+Nkunja@users.noreply.github.com>
2024-09-20 16:25:54 +0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://ellochallenge-1.onrender.com/',
   cache: new InMemoryCache()
 });
 
