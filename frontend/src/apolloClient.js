59b90cd90e0c381c99a2a73ac6eb555c408e0c05
Denis Nkunja <67855162+Nkunja@users.noreply.github.com>
2024-09-20 16:23:38 +0300

    Update apolloClient.js

--- frontend/src/apolloClient.js
+++ frontend/src/apolloClient.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 const httpLink = new HttpLink({
-  uri: process.env.REACT_APP_GRAPHQL_ENDPOINT,
+  uri: 'https://ellochallenge-1.onrender.com/',
 });
 
 
